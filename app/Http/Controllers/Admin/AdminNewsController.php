42d23476d83ba94bd9ab24d54956aa7413823913
scarymovie <81684836+scarymovie@users.noreply.github.com>
2021-04-08 17:38:50 +0300

    add app

---
+++ app/Http/Controllers/Admin/AdminNewsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminNewsController extends Controller
+{
+    public function create(){
+        echo"здесь надо будет тыкнуть чтобы создать статью";
+        exit;
+    }
+}
